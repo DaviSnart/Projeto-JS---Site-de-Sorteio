d555f254116b80fd21350fdfe1f9ca6d9e8d0ecd
DaviSnart <davi.soares9876543210@gmail.com>
2023-09-18 08:45:56 -0300

    exclusão do math.js e outras modificações de test de js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function generateNumber() {
     
+    const min  = document.querySelector (".input-min").value
+    const max = document.querySelector (".input-max").value
+
+    console.log (min, max)
 }
